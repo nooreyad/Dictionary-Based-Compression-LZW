fea8b784c775d1cde4e713aac6a95f597a6b1c49
nooreyad <nooreyadd39@gmail.com>
2023-11-03 02:49:50 +0200

    added main, compression, writeToFileCompression functions

---
+++ src/Pair.java
@@ -0,0 +1,14 @@
+public class Pair {
+    Integer number;
+    String str;
+    Pair(Integer number, String str){
+        this.number = number;
+        this.str = str;
+    }
+    public Integer getNumber(){
+        return number;
+    }
+    public String getStr(){
+        return str;
+    }
+}
